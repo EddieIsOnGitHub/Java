8145ba832b9b80a00cae63f7278dc03df89609f5
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-08-29 12:51:41 +0200

    new files

---
+++ ex_04_1_exercise/ShoppingCart.java
@@ -0,0 +1,21 @@
+
+package ex_04_1_exercise;
+
+public class ShoppingCart {
+    
+    public static void main(String[] args) {
+         // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "mary Smith";
+        String itemDesc = "shirt";
+        String message;
+
+
+        // Assign the message variable 
+        message = custName+" wants to purches "+ itemDesc;
+        
+        // Print and run the code
+        System.out.println(message);
+
+    }
+    
+}
