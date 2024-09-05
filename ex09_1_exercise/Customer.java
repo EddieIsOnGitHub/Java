6ea0cdb591ea4482da54b03a6c6b853efddfb0b0
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-09-05 09:25:53 +0200

    new files

---
+++ ex09_1_exercise/Customer.java
@@ -0,0 +1,15 @@
+package ex09_1_exercise;
+
+
+public class Customer {
+    public String name;
+    public String ssn;
+   
+    // Encapsulate this class.  Make ssn read only.
+    public String getName() {return name;}
+    public void setName(String n) {name = n;}
+
+    public String getSSN() {return ssn;}
+
+
+}
