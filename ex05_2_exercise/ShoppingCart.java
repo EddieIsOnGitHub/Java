73041ee44150b148ce1c504c351a2b7b598ecc5c
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-09-05 09:26:23 +0200

    update files

--- ex05_2_exercise/ShoppingCart.java
+++ ex05_2_exercise/ShoppingCart.java
@@ -16,7 +16,7 @@ public class ShoppingCart {
         System.out.println(message);
          
         // Print an element from the items array.  
-       
+        
         System.out.println(items[2]);
         //System.out.println(items[4]);
     }    
